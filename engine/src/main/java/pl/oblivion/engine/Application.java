e5bb65ad2aed7dd952f1a920cf4eae4c32c00803
PiotrIT2015 <piotr.prochnicki@gmail.com>
2018-10-14 22:00:00 +0200

    Extract window method

--- engine/src/main/java/pl/oblivion/engine/Application.java
+++ engine/src/main/java/pl/oblivion/engine/Application.java
@@ -1,4 +1,4 @@
-package pl.oblivion.engine;
+package pl.oblivion.app;
 
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
@@ -16,13 +16,12 @@ import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Application {
 
-  //todo uzyj tutaj klasy window
-  private long window; //dodaj w window
 
-  public void run() {
+
+public void run() {
     System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
-    init();
+    new Window().init();
     loop();
 
     glfwFreeCallbacks(window);
@@ -32,43 +31,7 @@ public class Application {
     glfwSetErrorCallback(null).free();
   }
 
-  private void init() {
-    GLFWErrorCallback.createPrint(System.err).set();
 
-    if (!glfwInit()) throw new IllegalStateException("Unable to initialize GLFW");
-
-    glfwDefaultWindowHints();
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
-    window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL); //dodaj w window width, height, title i uzyj
-    // tutaj
-    if (window == NULL) throw new RuntimeException("Failed to create the GLFW window");
-
-    glfwSetKeyCallback(
-        window,
-        (window, key, scancode, action, mods) -> {
-          if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-            glfwSetWindowShouldClose(window, true);
-        });
-
-    try (MemoryStack stack = stackPush()) {
-      IntBuffer pWidth = stack.mallocInt(1);
-      IntBuffer pHeight = stack.mallocInt(1);
-
-      glfwGetWindowSize(window, pWidth, pHeight);
-
-      GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-      glfwSetWindowPos(
-          window, (vidmode.width() - pWidth.get(0)) / 2, (vidmode.height() - pHeight.get(0)) / 2);
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-
-    glfwShowWindow(window);
-  }
 
   private void loop() {
     GL.createCapabilities();
@@ -81,4 +44,5 @@ public class Application {
       glfwPollEvents();
     }
   }
+  
 }
--- engine/src/main/java/pl/oblivion/engine/Window.java
+++ engine/src/main/java/pl/oblivion/engine/Window.java
@@ -1,5 +1,59 @@
-package pl.oblivion.engine;
+package pl.oblivion.app;
 
-public class Window {
-	//todo stworz klase window
-}
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window{
+	
+  private long window;
+	
+  private void init() {
+    GLFWErrorCallback.createPrint(System.err).set();
+
+    if (!glfwInit()) throw new IllegalStateException("Unable to initialize GLFW");
+
+    glfwDefaultWindowHints();
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+    if (window == NULL) throw new RuntimeException("Failed to create the GLFW window");
+
+    glfwSetKeyCallback(
+        window,
+        (window, key, scancode, action, mods) -> {
+          if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+            glfwSetWindowShouldClose(window, true);
+        });
+
+    try (MemoryStack stack = stackPush()) {
+      IntBuffer pWidth = stack.mallocInt(1);
+      IntBuffer pHeight = stack.mallocInt(1);
+
+      glfwGetWindowSize(window, pWidth, pHeight);
+
+      GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+      glfwSetWindowPos(
+          window, (vidmode.width() - pWidth.get(0)) / 2, (vidmode.height() - pHeight.get(0)) / 2);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    glfwShowWindow(window);
+  }	
+
+  
+}	
