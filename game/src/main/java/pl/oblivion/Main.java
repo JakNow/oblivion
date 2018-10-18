e746c177bd7bce16a5374160e295dcd82fb5cfd2
PiotrIT2015 <piotr.prochnicki@gmail.com>
2018-10-18 11:00:49 +0200

    pom-fix

--- game/src/main/java/pl/oblivion/Main.java
+++ game/src/main/java/pl/oblivion/Main.java
@@ -7,22 +7,15 @@ import pl.oblivion.engine.Application;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
-import java.lang.Throwable;
-
 @AppConfig("app.properties")
 public class Main {
     
   private static final Logger log = LogManager.getLogger(Main.class);
   
   public static void main(String[] args) {
-	
-		try{
+			
                     new AppConfigRunner();
 			new Application().run();
-		}catch(Throwable ex){
-			log.error("Sorry, something wrong!", ex);
-		}
-		
-		
+			
 	}
 }
