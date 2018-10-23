68947c7f9dac8b245b9244dab8864ce95b353cc8
PiotrIT2015 <piotr.prochnicki@gmail.com>
2018-10-23 15:12:35 +0200

    Fix/test

--- core/src/main/java/pl/oblivion/core/utils/GetSystemProperty.java
+++ core/src/main/java/pl/oblivion/core/utils/GetSystemProperty.java
@@ -12,79 +12,34 @@ import java.util.Properties;
  * @author Piotr
  */
 public class GetSystemProperty {
+       
+    public static Integer getInt(String key) {
+      Properties p = System.getProperties();
+      p.list(System.out);  
+        return Integer.getInteger(key);
+    }  
     
-    public static int getInt(String a, int b) {
-        Properties p = System.getProperties();
-        if(p.isEmpty()){
-        p.list(System.out);  
-      }else{
-        return b;  
-      }
-       return 0;
+    public static Float getFloat(String key) {
+      Properties p = System.getProperties();
+      p.list(System.out);  
+        return Float.parseFloat(key);
     }
-
-    public static int getInt(String a) {
-        int liczba = getInt(a, 0); // here, 0 is a default value for b
-      return liczba;
+    
+    public static Double getDouble(String key) {
+      Properties p = System.getProperties();
+      p.list(System.out);  
+        return Double.parseDouble(key);
     }
-
-            
-    public static float getFloat(String a, float b) {
-        Properties p = System.getProperties();
-        if(p.isEmpty()){
-        p.list(System.out);  
-      }else{
-        return b;  
-      }
-       return 0;
+    
+    public static char getChar(String key) {
+      Properties p = System.getProperties();
+      p.list(System.out);  
+        return key.charAt(0);
     }
-
-    public static float getFloat(String a) {
-        float liczba = getFloat(a, 0); // here, 0 is a default value for b
-      return liczba;
+    
+    public static String getString(String key) {
+      Properties p = System.getProperties();
+      p.list(System.out);  
+        return key;
     }
-
-    
-    
-        
-    public static double getDouble(String a, double b) {
-        Properties p = System.getProperties();
-        if(p.isEmpty()){
-        p.list(System.out);  
-      }else{
-        return b;  
-      }
-       return 0;
-    }
-
-    public static double getDouble(String a) {
-        double liczba = getDouble(a, 0); // here, 0 is a default value for b
-      return liczba;
-    }
-
-    
-    
-        
-    public static char getChar(String a, char b) {
-        Properties p = System.getProperties();
-        if(p.isEmpty()){
-        p.list(System.out);  
-      }else{
-        return b;  
-      }
-       return 0;
-    }
-
-    public static char getChar(String a) {
-        char znak = getChar(a, ' '); // here, 0 is a default value for b
-      return znak;
-    }
-
-    
-    
- // public static Integer getInt2(String key) {
-//     return Integer.getInteger(key);
-//    }  
-    
-    
 }
