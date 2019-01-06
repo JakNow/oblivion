53b57f101d6d1570aa32f2a774a704a019bc5cb9
JakNow <jakub.pawel.nowakowski@gmail.com>
2019-01-06 11:42:09 +0100

    Replaced AppConfig with OblivionConfiguration annotation. Changed loading logic.

--- common/src/main/java/pl/oblivion/common/annotations/MissingAppConfigAnnotationException.java
+++
@@ -1,8 +0,0 @@
-package pl.oblivion.common.annotations;
-
-public class MissingAppConfigAnnotationException extends RuntimeException {
-
-	public MissingAppConfigAnnotationException() {
-		super("Add @AppConfig annotation to main class");
-	}
-}
